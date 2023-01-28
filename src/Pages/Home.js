3ca3adb4534ae7aaeab762f5eb49162e867286f2
ROHITchin995 <rohitsure.9915@gmail.com>
2023-01-28 16:39:32 +0530

    css for homepage added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import PersonIcon from '@mui/icons-material/Person';
+import "./Home.css"
 export const Home = () => {
   return (
     <div className='home'>
