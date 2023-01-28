e39ea46d16360cf049196922dffca89c8431760c
ROHITchin995 <rohitsure.9915@gmail.com>
2023-01-28 16:31:23 +0530

    Home page layout done

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
         <Routes>
           <Route path='' element={<Home />} />
           <Route path='/search' element={<SearchPage />}>
-          
           </Route>
         </Routes>
       </Router>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,27 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
+import PersonIcon from '@mui/icons-material/Person';
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="home_header">
+        <div className="home_headerLeft">
+          <Link to="/about">About </Link>
+          <Link to="/store">Store </Link>
+        </div>
+      </div>
+      <div className="home_headerRight">
+        <Link to="/gmail">Gmail </Link>
+        <Link to="/images">Images </Link>
+        <PersonIcon />
+      </div>
+      <div className="home_body">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/368px-Google_2015_logo.svg.png" alt="" />
+        <div className="home_inputContainer">
+          Search
+        </div>
+      </div>
+    </div>
+
   )
 }
