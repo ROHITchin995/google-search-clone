a79ffb28742314c3351cacd966499b3edca0ed8f
ROHITchin995 <rohitsure.9915@gmail.com>
2023-01-28 15:15:48 +0530

    Route added to pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
-
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {SearchPage} from './Pages/SearchPage'
+import { Home } from './Pages/Home';
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path='' element={<Home />} />
+          <Route path='/search' element={<SearchPage />}>
+          
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const useGoogleSearch = () => {
-    const CONTEXT_KEY = "027f72849c06c4c0b"
+    // const CONTEXT_KEY = "027f72849c06c4c0b"
   return (
     <div>useGoogleSearch</div>
   )
